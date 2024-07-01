12251d2e8ba3adec7ba9d8207f59018e2183a592
Oscar Eduardo Miramontes Raygoza <oscar.eduardo.raygoza@gmail.com>
2024-06-30 22:16:28 -0600

    chore: rename base astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://oscar-raygoza.github.io',
-  base: '/minimalist-portfolio-json',
+  base: '/profile',
 });
